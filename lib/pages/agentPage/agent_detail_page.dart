54c62554d45d59ee282d3e9c3aed5fd090b47524
Alirexzaa <alirezagithubnew@gmail.com>
2024-08-10 22:25:04 +0330

    fix

--- lib/pages/agentPage/agent_detail_page.dart
+++ lib/pages/agentPage/agent_detail_page.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 import 'package:valorant_api/api/dart_api.dart';
